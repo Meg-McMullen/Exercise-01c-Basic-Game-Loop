3b18888acc19deb4e09addcdda9cf754545df562
Meg-McMullen <mcmullenmeggie@gmail.com>
2021-09-02 17:45:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,21 @@ response = ""
 while True:
     if response == "quit":
         break
+
     # Find passage (update)
     current_location = {}
+    for passage in world["passage"]:
+      if passage("name") == current:
+        current_location = passage
+
     # Display passage (render the world)
+    print(current_location["name"])
+    print(current_location["cleanText"])
+    for Link in current_location["Links"]:
+      print(link["linkText"])
+
     # Ask for response (get input)
+    response = input("Where do you want to go? ")
+    for link in current_location["links"]:
+      if response == link["linkText"]:
+        current = link["passageName"]
